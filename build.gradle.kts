3ff365fc1269e378331229286959a3c401b8054f
TurboJax <turbojax07@gmail.com>
2026-07-08 08:31:06 -0400

    Init Commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-library`
+    alias(libs.plugins.paperweight)
+    alias(libs.plugins.run.paper)
+}
+
+val javaVersion = (project.property("javaVersion") as String).toInt();
+val minecraftVersion = project.property("minecraftVersion");
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    paperweight.paperDevBundle("${minecraftVersion}+")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("${minecraftVersion}")
+        jvmArgs("-Xms2G", "-Xmx2G", "-Dlog4j.configurationFile=log4j2.xml")
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/java/org/turbojax/legendslib/LegendsLib.java
@@ -0,0 +1,19 @@
+package org.turbojax.legendslib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class LegendsLib extends JavaPlugin {
+    private static final Logger LOGGER = LoggerFactory.getLogger("LegendsLib");
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
