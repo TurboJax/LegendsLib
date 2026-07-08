f490fb55667dc2e8baeaad83f9c5f6ca028d8b02
TurboJax <turbojax07@gmail.com>
2026-07-08 14:36:12 -0400

    Incrementing player kill counts

---
+++ src/main/java/org/turbojax/legendslib/listeners/PlayerDeathListener.java
@@ -0,0 +1,35 @@
+package org.turbojax.legendslib.listeners;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.turbojax.legendslib.LegendaryWeapon;
+
+public class PlayerDeathListener implements Listener {
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Entity entity = event.getDamageSource().getDirectEntity();
+
+        // Handling if the entity isn't a player
+        if (!(entity instanceof Player)) {
+            entity = event.getDamageSource().getCausingEntity();
+
+            // Handling if the entity still isn't a player
+            if (!(entity instanceof Player)) return;
+        }
+
+        Player player = (Player) entity;
+
+        // Getting the item
+        ItemStack item = player.getInventory().getItemInMainHand();
+
+        // Making sure the item is a legendary weapon
+        if (!item.getPersistentDataContainer().has(LegendaryWeapon.INVENTORY_ABILITIES_KEY)) return;
+
+        // Increasing the kill count
+        LegendaryWeapon.incrementKillCount(item, player);
+    }
+}
